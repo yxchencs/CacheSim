618a34e154f418833ff8dfce5c8422f29caa61b1
YuxinChen <yuxinchencs@gmail.com>
2024-06-10 15:35:27 +0800

    update process_results_device_test.py

--- Caching-Policy/scripts/python/results_process/process_results_device_test.py
+++ Caching-Policy/scripts/python/results_process/process_results_device_test.py
@@ -6,6 +6,7 @@ import tqdm
 
 # 1GB/uniform/read_1/4KB/emmc
 def get_folder_list():
+    global disk_size_list
     global device_id_list
 
     for folder1 in os.listdir(util2.path_head):
@@ -17,17 +18,29 @@ def get_folder_list():
                     for folder3 in os.listdir(sub_dir2):
                         sub_dir3 = os.path.join(sub_dir2, folder3)
                         if os.path.isdir(sub_dir3):
-                            device_id_list.append(folder2)
-                            util2.operation_read_ratio_list.append(folder1)
-                            util2.block_size_list.append(folder3)
-
-    # print(f"util2.operation_read_ratio_list({len(util2.operation_read_ratio_list)}):{util2.operation_read_ratio_list}")
-    # print(f"device_id_list({len(device_id_list)}):{device_id_list}")
-    # print(f"util2.block_size_list({len(util2.block_size_list)}):{util2.block_size_list}")
+                            for folder4 in os.listdir(sub_dir3):
+                                sub_dir4 = os.path.join(sub_dir3, folder4)
+                                if os.path.isdir(sub_dir4):
+                                    for folder5 in os.listdir(sub_dir4):
+                                        sub_dir5 = os.path.join(sub_dir4, folder5)
+                                        if os.path.isdir(sub_dir5):
+                                            disk_size_list.append(folder1)
+                                            util2.workload_type_list.append(folder2)
+                                            util2.operation_read_ratio_list.append(folder3)
+                                            util2.block_size_list.append(folder4)
+                                            device_id_list.append(folder5)
+    if util2.PRINT_INFO:
+        print(f"disk_size_list({len(disk_size_list)}):{disk_size_list}")
+        print(f"workload_type_list({len(util2.workload_type_list)}):{util2.workload_type_list}")
+        print(
+            f"util2.operation_read_ratio_list({len(util2.operation_read_ratio_list)}):{util2.operation_read_ratio_list}")
+        print(f"util2.block_size_list({len(util2.block_size_list)}):{util2.block_size_list}")
+        print(f"device_id_list({len(device_id_list)}):{device_id_list}")
     print('done get folders')
 
 
 def process_results_for_device_test():
+    global disk_size_list
     global device_id_list
 
     list_total, list_p99, list_avg_latency = [], [], []
@@ -40,13 +53,16 @@ def process_results_for_device_test():
     list_sd_read_nums, list_sd_read_avg_latency, list_sd_read_p99 = [], [], []
     list_sd_write_nums, list_sd_write_avg_latency, list_sd_write_p99 = [], [], []
 
-
-    for i in tqdm.trange(len(util2.operation_read_ratio_list)):
+    for i in tqdm.trange(len(disk_size_list)):
+        disk_size = disk_size_list[i]
+        disk_size_unit = disk_size[-2:]
+        workload_type = util2.workload_type_list[i]
         operation_read_ratio = util2.operation_read_ratio_list[i]
-        device_id = device_id_list[i]
         block_size = util2.block_size_list[i]
+        device_id = device_id_list[i]
 
-        file_path_begin = os.path.join(util2.path_head, operation_read_ratio, device_id, block_size)
+        file_path_begin = os.path.join(util2.path_head, disk_size, workload_type, operation_read_ratio, block_size,
+                                       device_id)
         if util2.PRINT_INFO:
             print('data process:', file_path_begin)
 
@@ -100,6 +116,9 @@ def process_results_for_device_test():
         list_sd_write_avg_latency.append(sd_write_avg_latency)
         list_sd_write_p99.append(sd_write_p99)
 
+    list_disk_size = [util2.convert_to_numeric(x) for x in disk_size_list]
+    list_block_size = [util2.convert_to_numeric(x) for x in util2.block_size_list]
+    list_operation_read_ratio = [util2.convert_to_numeric(x) for x in util2.operation_read_ratio_list]
     list_energy = [a * b if a is not None and b is not None else None for a, b in zip(list_avg_power, list_total)]
     list_emmc_kb_read = [x / 1024 for x in list_emmc_kb_read]
     list_emmc_kb_wrtn = [x / 1024 for x in list_emmc_kb_wrtn]
@@ -108,14 +127,17 @@ def process_results_for_device_test():
     dict_device_id = {'sd': 'SD', 'emmc': "eMMC"}
     device_id_list = [dict_device_id[x] for x in device_id_list]
 
-    data = {'Operation Read Ratio': util2.operation_read_ratio_list, 'Device ID': device_id_list,
-            'Block Size(KB)': util2.block_size_list,
+    data = {f'Disk Size({disk_size_unit})': list_disk_size,
+            'Workload Type': util2.workload_type_list,
+            'Operation Read Ratio': list_operation_read_ratio,
+            'Block Size(KB)': list_block_size,
+            'Device ID': device_id_list,
             'Average Latency(ms)': list_avg_latency, 'P99 Latency(ms)': list_p99,
-            'Average CPU Usage(%)': list_cpu_usage, 'Average Memory Used(MB)': list_mem_used,
             'Total Time(s)': list_total, "Bandwidth(MB/s)": list_bandwidth,
-            'Average Power(W)': list_avg_power, 'Energy(J)': list_energy,
+            'Average CPU Usage(%)': list_cpu_usage, 'Average Memory Used(MB)': list_mem_used,
             'eMMC Read Size(KB)': list_emmc_kb_read, 'eMMC Write Size(KB)': list_emmc_kb_wrtn,
             'SD Read Size(KB)': list_sd_kb_read, 'SD Write Size(KB)': list_sd_kb_wrtn,
+            'Average Power(W)': list_avg_power, 'Energy(J)': list_energy,
             'eMMC Read Numbers': list_emmc_read_nums, 'eMMC Read Average Latency(ms)': list_emmc_read_avg_latency,
             'eMMC Read P99 Latency(ms)': list_emmc_read_p99,
             'eMMC Write Numbers': list_emmc_write_nums, 'eMMC Write Average Latency(ms)': list_emmc_write_avg_latency,
@@ -137,8 +159,9 @@ def process_device_test():
     process_results_for_device_test()
 
 
+disk_size_list = []
 device_id_list = []
 
 if __name__ == '__main__':
-    util2.path_head = 'E:\projects\cs\Caching-Policy\Caching-Policy\scripts\python/anlysis\data\statistic/2024-05-07_01-23-13_device_test'
+    util2.path_head = 'E:\projects/records/2024-06-10_11-25-29_device_uniform_latest_zipfian'
     process_device_test()
