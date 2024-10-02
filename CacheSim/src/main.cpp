61bd584bd3679754bc1deacc5fbbdd06b8a9be90
YuxinChen <yuxinchencs@gmail.com>
2024-10-02 17:53:53 +0800

    add sample data

--- CacheSim/src/main.cpp
+++ CacheSim/src/main.cpp
@@ -27,17 +27,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-/*Terminal 1
-cd src
-sudo mount /dev/mmcblk0p1 /mnt/eMMC
-lsblk
-sudo date -s "YYYY-YY-DD HH:mm:ss"
-sudo g++ -g -std=c++17 -o main main.cpp
-sudo valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes ./main
-sudo ./main
-*/
-/*Terminal 2
-cd scripts/bash
-sudo bash cpu_mem_disk.sh
-*/
